de7fe1814b5a44e46bc1989ca81e9fe331ae2e73
stepguest <stepguest@STHQ012E-14>
2023-03-28 10:25:21 +0400

    Add project files.

---
+++ wpf style/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_style
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<string> myList { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            myList = new ObservableCollection<string>();
+            myList.Add("asd");
+            myList.Add("bsd");
+            myList.Add("fsd");
+
+            myList.CollectionChanged += MyList_CollectionChanged;
+        }
+
+        private void MyList_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
